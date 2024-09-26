916fa95deeae93d2feef48cf194ee86c0dca6305
Demis Lavrentidis <demilavr@gmail.com>
2024-09-26 23:51:49 +0100

    * added getPhotos test successful response

--- app/src/test/java/com/hometask/flickrapp/viewmodel/FlickrViewModelTest.kt
+++ app/src/test/java/com/hometask/flickrapp/viewmodel/FlickrViewModelTest.kt
@@ -1,50 +1,100 @@
 package com.hometask.flickrapp.viewmodel
 
+import com.hometask.flickrapp.model.FlickrModelDto
+import com.hometask.flickrapp.model.MainPhotoDto
+import com.hometask.flickrapp.model.Photo
 import com.hometask.flickrapp.repository.FlickrRepo
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
 import io.mockk.mockkConstructor
-import junit.framework.TestCase.assertTrue
+import io.mockk.unmockkAll
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import retrofit2.Response
+
 
 @ExperimentalCoroutinesApi
 @RunWith(JUnit4::class)
 class FlickrViewModelTest {
 
 
-
     private lateinit var repository: FlickrRepo
     private lateinit var viewModel: FlickrViewModel
-    private val dispatcher = StandardTestDispatcher()
+
+    private val testDispatcher = StandardTestDispatcher()
 
     @Before
     fun setUp() {
-        repository = FlickrRepo()
+        Dispatchers.setMain(testDispatcher)
+        repository = mockk()
         mockkConstructor(FlickrRepo::class)
+        every { anyConstructed<FlickrRepo>() } returns repository
         viewModel = FlickrViewModel()
     }
 
-    @Test
-    fun `test getPhotos with successful response`()  {
-        Dispatchers.setMain(dispatcher)
-        viewModel.getPhotos()
-        dispatcher.scheduler.runCurrent()
-
-        assertTrue(viewModel.uiState.value.photos.isNotEmpty())
+    @After
+    fun tearDown() {
         Dispatchers.resetMain()
+        unmockkAll()
     }
 
     @Test
-    fun onEvent() {
-    }
+    fun `test getPhotos with successful response`() = runTest {
+        // Arrange
+        val mockResponse = mockk<Response<MainPhotoDto>>()
+        val mockFlickrResponse = mockk<MainPhotoDto>()
+        val mockPhotos = mockk<FlickrModelDto>()
+        val mockPhotoList = listOf(
+            Photo(
+                id = "1",
+                owner = "2",
+                secret = "3",
+                server = "4",
+                farm = 5,
+                title = "6",
+                ispublic = 7,
+                isfriend = 8,
+                isfamily = 9,
+                iconserver = "10",
+                iconfarm = 11,
+                ownername = "12",
+                datetaken = "13",
+                description = mockk(),
+            )
+        )
 
-    @Test
-    fun getPhotos() {
+        every { mockResponse.isSuccessful } returns true
+        every { mockResponse.body() } returns mockFlickrResponse
+        every { mockFlickrResponse.photos } returns mockPhotos
+        every { mockPhotos.photo } returns mockPhotoList
+        coEvery { repository.getPhotosWithTags() } returns mockResponse
+
+        // Act
+        viewModel.getPhotos()
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        // Assert
+        assertEquals(mockPhotoList, viewModel.uiState.value.photos)
+        coVerify { repository.getPhotosWithTags() }
     }
 }
+
+//    @Test
+//    fun onEvent() {
+//    }
+//
+//    @Test
+//    fun getPhotos() {
+//    }
