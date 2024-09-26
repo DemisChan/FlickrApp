eea046ebc1b4eeaacc682b788fc286e14cff7dfa
Demis Lavrentidis <demilavr@gmail.com>
2024-09-26 22:49:28 +0100

    * added getPhotos test

--- app/src/test/java/com/hometask/flickrapp/viewmodel/FlickrViewModelTest.kt
+++ app/src/test/java/com/hometask/flickrapp/viewmodel/FlickrViewModelTest.kt
@@ -1,10 +1,13 @@
 package com.hometask.flickrapp.viewmodel
 
+import com.hometask.flickrapp.repository.FlickrRepo
+import io.mockk.mockkConstructor
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import org.junit.Assert.*
-
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,14 +17,27 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class FlickrViewModelTest {
 
+
+
+    private lateinit var repository: FlickrRepo
     private lateinit var viewModel: FlickrViewModel
     private val dispatcher = StandardTestDispatcher()
+
     @Before
     fun setUp() {
+        repository = FlickrRepo()
+        mockkConstructor(FlickrRepo::class)
+        viewModel = FlickrViewModel()
     }
 
     @Test
-    fun getUiState() {
+    fun `test getPhotos with successful response`()  {
+        Dispatchers.setMain(dispatcher)
+        viewModel.getPhotos()
+        dispatcher.scheduler.runCurrent()
+
+        assertTrue(viewModel.uiState.value.photos.isNotEmpty())
+        Dispatchers.resetMain()
     }
 
     @Test
